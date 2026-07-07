e0273023f1dabc66523a03b043b60506e3e22588
Maramreddy Shiva Nandheswara Reddy <mnandu0011@gmail.com>
2026-07-07 14:06:09 +0530

    Create Node class for student management
    Added a Node class to store student details using linked list.

--- student_management.py
+++ student_management.py
@@ -1,1 +1,7 @@
-
+#creating the class for store marks,name and roll number using the linkedlist.
+class Node:
+  def __init__(self,name,roll_number,marks):
+    self.name=name
+    self.marks=marks
+    self.roll_number=roll_number
+    self.next=None
